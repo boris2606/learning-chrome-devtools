25f14ed06b58f97d2603400602697f8e437dd892
Boris <boris260692@gmail.com>
2022-05-26 18:14:40 +0300

    Some changes

---
+++ src/user.js
@@ -0,0 +1,73 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const num1 = document.querySelector('#num1')
+    const num2 = document.querySelector('#num2')
+    const addBtn = document.querySelector('#add')
+    const subBtn = document.querySelector('#sub')
+    const delBtn = document.querySelector('#del')
+    const mnozBtn = document.querySelector('#mnoz')
+    const clearBlockBtn = document.querySelector('.clear')
+    const output = document.querySelector('#output')
+  
+    function getInputValues() {
+      const value1 = parseInt(num1.value)
+      const value2 = +num2.value
+  
+      return [value1, value2]
+    }
+  
+  
+    function addHandler() {
+      const values = getInputValues()
+      // console.log(values)
+      const result = values[0] + values[1]
+      displayResult(result)
+    }
+  
+    function subHandler() {
+      const values = getInputValues()
+      const result = values[0] - values[1]
+      displayResult(result)
+    }
+
+    function delHandler() {
+        const values = getInputValues()
+        const result = values[0] / values[1]
+        displayResult(result)
+    }
+
+    function mnozHandler() {
+        const values = getInputValues()
+        const result = values[0] * values[1]
+        displayResult(result)
+    }
+  
+    function displayResult(result) {
+      output.closest('.card').style.display = 'flex'
+      output.innerHTML = `Результат = ${result}`
+      console.trace()
+    }
+
+    function clearBlock() {
+        output.closest('.card').style.display = 'none'
+        num1.value = ''
+        num2.value = ''
+    }
+  
+  
+    // console.log('Test', addBtn)
+  
+    addBtn.addEventListener('click', addHandler)
+    subBtn.addEventListener('click', subHandler)
+    mnozBtn.addEventListener('click', mnozHandler)
+    delBtn.addEventListener('click', delHandler)
+    clearBlockBtn.addEventListener('click',clearBlock)
+  
+  })
+  
+  
+//   setTimeout(() => {
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+//   }, 5000)
